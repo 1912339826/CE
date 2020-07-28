e650fe84c5a33081743cebf9853121aaa3bb1160
username@email.com <1912339826@qq.com>
2020-07-28 17:30:12 +0800

    no message

---
+++ MYDome/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div>
+      <img :src="item" v-for="(item, index) in arr" :key="index" style="width:50vw;"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      arr: [],
+    };
+  },
+  created() {
+    for (let index = 0; index < 513; index++) {
+      this.arr.push(require(`../../images/100 (${index + 1}).gif`));
+    }
+  },
+  mounted() {},
+};
+</script>
+
+<style lang="less">
+// less外部文件的使用
+@import url("./assets/less/index");
+</style>
---
+++ MYDome/src/pages/home/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="home">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:"home",
+  components: {
+      
+  },
+  props: {},
+  data() {
+    return {
+
+    };
+  },
+  created() {},
+  mounted() {},
+  activated() {},
+  update() {},
+  methods: {
+
+  },
+  filters: {},
+  computed: {},
+  watch: {},
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
